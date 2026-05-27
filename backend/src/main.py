d54f2a40ef2d93e9519425e427833ff184b0b23c
sncthyz <andikacahyarahman123@gmail.com>
2026-05-27 20:47:47 +0700

    fourth

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory="frontend/src/templates")
 
 @app.get("/")
 def index(request:Request):
