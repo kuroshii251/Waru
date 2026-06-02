8213777f223682cdaa368c442f4371ad32135af0
sncthyz <andikacahyarahman123@gmail.com>
2026-06-02 16:09:05 +0700

    lastttttsssts

--- backend/src/utils/db.py
+++ backend/src/utils/db.py
@@ -1,5 +1,5 @@
 import os
-
+from urllib.parse import urlparse
 from dotenv import load_dotenv
 from fastapi_asyncpg import configure_asyncpg
 
@@ -8,10 +8,17 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 print("DATABASE_URL EXISTS:", DATABASE_URL is not None)
+parsed = urlparse(DATABASE_URL)
+
+print("DB HOST =", parsed.hostname)
+print("DB PORT =", parsed.port)
 
 def setup_database(app):
-    db = configure_asyncpg(app, DATABASE_URL)
-
+db = configure_asyncpg(
+    app,
+    DATABASE_URL,
+    ssl="require"
+)
     @db.on_init
     async def _(conn):
         print("Database connected")
